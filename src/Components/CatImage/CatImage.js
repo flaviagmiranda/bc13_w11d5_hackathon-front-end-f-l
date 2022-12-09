f3cf8471dcd24f93869fc18bb70d2c9e6376b505
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-12-09 12:08:30 +0000

    useeffect removed

--- src/Components/CatImage/CatImage.js
+++ src/Components/CatImage/CatImage.js
@@ -1,17 +1,7 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 export default function CatImage({ cats }) {
-
-    const [url, setUrl] = useState("")
-
-    useEffect(() => {
-        async function constructUrl() {
-            setUrl(`https://cataas.com/cat/${cats[0]?.id}`)   
-        }
-        constructUrl();
-      }, [cats]);
-
   return (
-    <img src={url} alt="cat" />
+    <img src={`https://cataas.com/cat/${cats[0]?.id}`} alt="cat" />
   )
-}
+};
